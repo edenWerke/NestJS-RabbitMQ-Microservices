9244ec4612d5b3c04886e9ca2bd5e54c84cc434b
edenWerke <edenw6292@gmail.com>
2026-08-29 01:04:01 -0700

    add

---
+++ orders-service/drizzle/0000_mute_midnight.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "orders" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"customer_name" varchar(100) NOT NULL,
+	"item" varchar(100) NOT NULL,
+	"quantity" integer NOT NULL,
+	"status" varchar(50) DEFAULT 'pending' NOT NULL
+);
