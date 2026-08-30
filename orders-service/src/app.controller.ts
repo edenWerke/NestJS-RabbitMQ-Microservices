1f4077b314225b4033e82584168cea1c751169b1
edenWerke <edenw6292@gmail.com>
2026-08-30 01:16:51 -0700

    add

--- orders-service/src/app.controller.ts
+++ orders-service/src/app.controller.ts
@@ -1,5 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+// a structure for the data that we want to send between parts of our application
+export class CreateOrderDto{
+  customerName!:string;
+  item!:string;
+  quantity!:number;
+}
+
 
 @Controller()
 export class AppController {
--- orders-service/src/app.module.ts
+++ orders-service/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+// "RabbitMQ, please remember this queue even if you restart."
 @Module({
   imports: [
     ClientsModule.register([
