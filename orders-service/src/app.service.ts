1fafa87ffbe36be8f812e292c35185ab2128ceae
edenWerke <edenw6292@gmail.com>
2026-08-30 02:39:04 -0700

    add

--- orders-service/src/app.service.ts
+++ orders-service/src/app.service.ts
@@ -7,17 +7,25 @@ import { orders } from './db/schema';
 @Injectable()
 export class AppService {
   constructor(
-    // "NestJS, give me the thing that you registered under the name KITCHEN_SERVICE."
+    // "NestJS, give me the thing registered as KITCHEN_SERVICE."
     @Inject('KITCHEN_SERVICE')
     private readonly kitchenClient: ClientProxy,
   ) {}
-  async createOrder(dto:CreateOrderDto){
-  const {}=await db.insert(orders).values({
-    customerName:dto.customerName,
-    item:dto.item,
-    quantity:dto.quantity,
 
-    status:"pending"
-  })
+  async createOrder(dto: CreateOrderDto) {
+    const [order] = await db
+      .insert(orders)
+      .values({
+        customerName: dto.customerName,
+        item: dto.item,
+        quantity: dto.quantity,
+        status: 'pending',
+      })
+      .returning();
+
+    console.log(`Order saved to DB: ${order.id}`);
+    // this.kitchenClient.emit{'order created',}
+
+    return order;
   }
 }
