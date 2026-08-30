497a4b21570dd63c59c7d2ea5ec4898dd6181a79
edenWerke <edenw6292@gmail.com>
2026-08-30 01:08:43 -0700

    add

--- orders-service/src/app.module.ts
+++ orders-service/src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ClientsModule.register({
-      name:"KITCHEN_SERVICE",
-      transport:"Transport.RMQ",
-      optios:{
-        urls:{'amqp://guest@localhost:5672'},
-        queue:"kitchen_queue"
-      }
-    })
+    ClientsModule.register([
+      {
+        name: 'KITCHEN_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: 'kitchen_queue',
+          queueOptions: {
+            durable: process.env.NODE_ENV === 'production',
+          },
+        },
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
