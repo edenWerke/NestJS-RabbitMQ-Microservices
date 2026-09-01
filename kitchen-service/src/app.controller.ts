5110fa8a4987634a3a61b06f1585b6c40ceb37f2
edenWerke <edenw6292@gmail.com>
2026-09-01 10:56:03 +0300

    ass

--- kitchen-service/src/app.controller.ts
+++ kitchen-service/src/app.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-@EventPattern('order_created')
-async handleOrderCreated(@Payload( data:{}){
-       orderId: string,
-      customerName:string,
-      item: string,
-      quantity: number,
-}){
-  console.log('kitchen recived order: + order.id')
-}
-}
+  @EventPattern('order_created')
+  async handleOrderCreated(
+    @Payload()
+    data: {
+      orderId: string;
+      customerName: string;
+      item: string;
+      quantity: number;
+    },
+  ) {
+    console.log(`Kitchen received order: ${data.orderId}`);
+  }
