a090bbb4e6dc63c2de43370ac3cbed62e73c7d6a
edenWerke <edenw6292@gmail.com>
2026-09-02 22:25:58 +0300

    app controller

--- kitchen-service/src/app.controller.ts
+++ kitchen-service/src/app.controller.ts
@@ -1,7 +1,6 @@
-
 import { Controller } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -17,8 +16,8 @@ export class AppController {
       quantity: number;
     },
   ) {
-    console.log(`Kitchen received order: ${data.orderId}`);
-    await this.appService.processOrder();
+    console.log('Kitchen received order: ' + data.orderId);
+
+    await this.appService.processOrder(data);
   }
 }
-
